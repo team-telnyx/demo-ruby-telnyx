5ef81d8c427c8dfdf44b01e458a755fd8c110702
Dan Tolbert <dant@telnyx.com>
2022-01-26 11:02:12 -0500

    add pagination to list numbers example

--- list-phone-numbers/app.rb
+++ list-phone-numbers/app.rb
@@ -1,9 +1,36 @@
 require "telnyx"
 require 'dotenv/load'
 
-def list_numbers()
+def list_all_numbers()
   begin
-    (Telnyx::PhoneNumber.list()).data
+    page_number = 1
+    page_size = 250
+    phone_number_response = list_numbers(page_number,page_size)
+    current_page = phone_number_response.meta.page_number
+    total_pages = phone_number_response.meta.total_pages
+    phone_number_list = phone_number_response.data
+    while current_page < total_pages
+      page_number = page_number + 1
+      phone_number_response = list_numbers(page_number,page_size)
+      current_page = phone_number_response.meta.page_number
+      total_pages = phone_number_response.meta.total_pages
+      phone_number_list.push(*phone_number_response.data)
+    end
+    return phone_number_list
+  rescue Exception => ex
+    puts "Exception listing numbers"
+    puts ex
+    exit
+  end
+end
+
+def list_numbers(page_number, size)
+  begin
+    page = {
+      number: page_number,
+      size: size
+    }
+    phone_numbers = Telnyx::PhoneNumber.list(page: page)
   rescue Exception => ex
     puts "Exception listing numbers"
     puts ex
@@ -22,7 +49,9 @@ end
 if __FILE__ == $0
   TELNYX_API_KEY=ENV.fetch('TELNYX_API_KEY')
   Telnyx.api_key = TELNYX_API_KEY
-  numbers = list_numbers()
+  numbers = list_all_numbers()
   numbers_with_voice = list_numbers_with_voice(numbers);
+  # puts(numbers_with_voice.length)
   numbers_with_messaging = list_numbers_with_messaging(numbers);
+  # puts(numbers_with_messaging.length)
 end
